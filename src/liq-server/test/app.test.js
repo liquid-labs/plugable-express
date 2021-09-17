57494c8bcf7ddc14ccdfb9ec7fa5a6178026d16c
Zane Rockenbaugh <zane@liquid-labs.com>
2021-09-17 11:41:48 -0500

    added app init test
    manually verified this would have caught previously fixed error where 'console' default was not set in 'app.initialize'

--- src/liq-server/test/app.test.js
+++ src/liq-server/test/app.test.js
@@ -17,21 +17,49 @@ const projectA01Package = {
 }
 
 describe('app', () => {
-  beforeAll(() => {
-    model.initialize({
-      LIQ_PLAYGROUND_PATH : simplePlaygroundPath,
-      reporter
+  describe('default setup provides useful info', () => {
+    const consoleLog = console.log
+    const logs = []
+    
+    beforeAll(() => {
+      model.initialize({
+        LIQ_PLAYGROUND_PATH : simplePlaygroundPath,
+        reporter
+      })
+      console.log = jest.fn((msg) => { logs.push(msg) })
+      app.initialize({ model })
     })
-    app.initialize({ model, reporter })
+    
+    afterAll(() => {
+      console.log = consoleLog
+    })
+    
+    test('describes registered paths', () => {
+      expect(logs.filter((msg) =>
+        msg.match(/registering handler.+[A-Z]+:\/[a-zA-Z0-9/-]*$/)).length)
+        .toBe(6)
+    })
+    
+    // TODO: use http.METHODS to verify that all registered paths used known verbs
   })
+  
+  describe('response testing', () => {
+    beforeAll(() => {
+      model.initialize({
+        LIQ_PLAYGROUND_PATH : simplePlaygroundPath,
+        reporter
+      })
+      app.initialize({ model, reporter })
+    })
 
-  test.each`
-  path | result
-  ${'/playground/projects/orgA/projectA01/packageJSON'} | ${projectA01Package}
-  ${'/playground/orgs/orgA/projects/projectA01/packageJSON'} | ${projectA01Package}
-  `("'GET $path' gets the package.json contents", async({ path, result }) => {
-    const { status, body } = await request(app).get(path)
-    expect(status).toBe(200)
-    expect(body).toEqual(result)
+    test.each`
+    path | result
+    ${'/playground/projects/orgA/projectA01/packageJSON'} | ${projectA01Package}
+    ${'/playground/orgs/orgA/projects/projectA01/packageJSON'} | ${projectA01Package}
+    `("'GET $path' gets the package.json contents", async({ path, result }) => {
+      const { status, body } = await request(app).get(path)
+      expect(status).toBe(200)
+      expect(body).toEqual(result)
+    })
   })
 })
