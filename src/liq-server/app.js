ccc4af828dbefe70d2da97fce478b6169bd52fb2
Zane Rockenbaugh <zane@liquid-labs.com>
2021-09-21 13:21:34 -0500

    make cache available to handlers

--- src/liq-server/app.js
+++ src/liq-server/app.js
@@ -4,11 +4,12 @@ import express from 'express'
 import { handlers } from './handlers'
 
 const app = express()
+const cache = new WeakMap()
 
 app.initialize = ({ model, reporter = console }) => {
   for (const handler of handlers) {
     reporter.log(`registering handler for path: ${handler.verb.toUpperCase()}:${handler.path}`)
-    app[handler.verb](handler.path, handler.func({ model }))
+    app[handler.verb](handler.path, handler.func({ cache, model }))
   }
   
   return app
