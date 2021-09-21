5f3116772581d49d7e87b1d66d62c8311c4847bc
Zane Rockenbaugh <zane@liquid-labs.com>
2021-09-21 18:46:19 -0500

    implements stop

--- src/cli/actions/status.sh
+++ src/cli/actions/status.sh
@@ -10,19 +10,19 @@ liq-server-status() {
     if pgrep -q -F "${LIQ_SERVER_PID_FILE}"; then
       liq-server-lib-is-up \
         && { [[ -n "${QUIET}" ]] || echo running; return ${LIQ_SERVER_STATUS_RUNNING}; } \
-        || { [[ -n "${QUIET}" ]] || echo starting; return ${LIQ_SERVER_STATUS_STARTING}; }
+        || { [[ -n "${QUIET}" ]] || echo starting; return ${LIQ_SERVER_STATUS_WORKING}; }
     else # PID file, but not running
       rm "${LIQ_SERVER_PID_FILE}"
       [[ -n "${QUIET}" ]] || echo stopped
       return ${LIQ_SERVER_STATUS_STOPPED}
     fi
-  elif pgrep -q -f liq-server.js; then
+  elif pgrep -q ${LIQ_SERVER_PGREP_MATCH}; then
     if liq-server-lib-is-up; then
-      echowarn "It looks like the server may be running (process $(pgrep -f liq-server.js)), but no pidfile was found. However, the server is responsive and appears to be running properly."
+      echowarn "It looks like the server may be running (process $(pgrep ${LIQ_SERVER_PGREP_MATCH})), but no pidfile was found. However, the server is responsive and appears to be running properly."
       [[ -n "${QUIET}" ]] || echo recoverable
       return ${LIQ_SERVER_STATUS_RECOVERABLE}
     else
-      echoerr "It looks like the server may be running (process $(pgrep -f liq-server.js)), but no pidfile was found. The server was also unresponsive and should be stopped."
+      echoerr "It looks like the server may be running (process $(pgrep ${LIQ_SERVER_PGREP_MATCH})), but no pidfile was found. The server was also unresponsive and should be stopped."
       [[ -n "${QUIET}" ]] || echo unrecoverable
       return ${LIQ_SERVER_STATUS_UNRECOVERABLE}
     fi
