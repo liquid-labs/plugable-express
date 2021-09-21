68e1ec3c90c05917840b860c46a3e164c3c4f1ca
Zane Rockenbaugh <zane@liquid-labs.com>
2021-09-21 17:55:41 -0500

    return rather than exit
    this lets us call status as a function as well as a terminal action

--- src/cli/actions/status.sh
+++ src/cli/actions/status.sh
@@ -9,23 +9,23 @@ liq-server-status() {
   if [[ -f "${LIQ_SERVER_PID_FILE}" ]]; then
     if pgrep -q -F "${LIQ_SERVER_PID_FILE}"; then
       liq-server-lib-is-up \
-        && { echo running; exit 0; } \
-        || { echo starting; exit 102; }
+        && { echo running; return 0; } \
+        || { echo starting; return 102; }
     else # PID file, but not running
       rm "${LIQ_SERVER_PID_FILE}"
       echo stopped
-      exit 101
+      return 101
     fi
   elif pgrep -q -f liq-server.js; then
     if liq-server-lib-is-up; then
       echowarn "It looks like the server may be running (process $(pgrep -f liq-server.js)), but no pidfile was found. However, the server is responsive and appears to be running properly."
-      exit 103
+      return 103
     else
       echoerr "It looks like the server may be running (process $(pgrep -f liq-server.js)), but no pidfile was found. The server was also unresponsive and should be stopped."
-      exit 104
+      return 104
     fi
   else
     echo stopped
-    exit 101
+    return 101
   fi
 }
