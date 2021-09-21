b4a6c906408f5fc38d7f064ac3cfd89724cabe9a
Zane Rockenbaugh <zane@liquid-labs.com>
2021-09-21 14:17:28 -0500

    implement GET:/ version info

--- src/liq-server/handlers/get.js
+++ src/liq-server/handlers/get.js
@@ -1,8 +1,46 @@
+import * as fs from 'fs'
+import * as os from 'os'
+import * as sysPath from 'path'
+
+import { safeJSONParse } from '../lib/load-playground'
+
 const verb = 'get'
 const path = '/'
 
 const func = ({ cache }) => (req, res) => {
-  res.send('TODO')
+  let versionInfo = cache.get(func) // we assume versions are stable while running
+  if (versionInfo === undefined) {
+    // Note, you might think we could use the playground, but the server might be running as an installed package.
+    const pkgJSON = safeJSONParse(sysPath.join(__dirname, '../package.json'))
+    if (pkgJSON === null) {
+      res.status(500).send("Could not locate server's package definition; installation may be corrupted.")
+      return
+    }
+    
+    versionInfo = {
+      server: pkgJSON.version,
+      node: process.version,
+      fullNode: process.versions,
+      platform: `${os.type()} ${os.release()}`,
+      fullPlatform: {
+        platform: os.platform(),
+        type: os.type(),
+        version: os.version(),
+        release: os.release()
+      }
+    }
+    cache.set(func, versionInfo)
+  }
+  
+  if (req.accepts('json')) {
+    res.json(versionInfo)
+  }
+  else if (req.accepts('text')) {
+    res.send(`liq-server: ${versionInfo.server}\nnode: ${versionInfo.node}\nplatform:${versionInfo.platform}\n`)
+  }
+  else {
+    res.status(406).send("The server does not support any response format acceptable to the client. Try one or more of:\nAccept: application/json'\nAccept: text/plain")
+  }
 }
 
 export { func, path, verb }
