7e7571d50ac237f2d71db1e923e04567d90d0923
Zane Rockenbaugh <zane@liquid-labs.com>
2021-09-26 19:08:27 -0500

    basic iteration to find org defs

--- src/liq-server/lib/load-orgs.js
+++ src/liq-server/lib/load-orgs.js
@@ -1,5 +1,28 @@
-const loadOrgs = ({ reporter }) => {
+const loadOrg = ({ projectModel, reporter }) => {
+  reporter.log(`Found org definition project: ${projectModel.fullName}`)
+}
+
+const loadOrgs = ({ playground, reporter = console }) => {
+  reporter.log('Loading organization data...')
   
+  for (const orgName of playground.orgsAlphaList) {
+    const orgTypeRe = /(^|[|])org([|]|$)/
+    const orgProjects = Object.values(playground.orgs[orgName].projects)
+      .filter((projectModel) => {
+        // reporter.log(projectModel)
+        return projectModel.packageJSON?.liq?.packageType?.match(orgTypeRe)
+      })
+    
+    if (!orgProjects || orgProjects.length === 0) {
+      reporter.log(`Skipping '${orgName}'; no org definition found.`)
+    }
+    else if (orgProjects.length > 1) {
+      reporter.error(`Skipping org '${orgName}' due to multiple, ambiguous definitions: ${orgProjects}`)
+    }
+    else {
+      loadOrg({ projectModel: orgProjects[0], reporter })
+    }
+  }
 }
 
 export { loadOrgs }
