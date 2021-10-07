ae80043ad29177cdd654b43bf303b9af77784f52
Zane Rockenbaugh <zane@liquid-labs.com>
2021-10-07 17:06:51 -0500

    switched verb to put

--- src/liq-server/handlers/orgs/staff/put.js
+++ src/liq-server/handlers/orgs/staff/put.js
@@ -1,11 +1,14 @@
-const verb = 'post'
+const verb = 'put'
 const path = '/orgs/:orgName/staff'
 
 const func = ({ model }) => (req, res) => {
   const results = []
   for (const fileName of Object.keys(req.files)) {
+    
+    
     results.push(Object.keys(req.files[fileName]))
   }
+  results.push(req.param)
   res.json(results)
   /*for (const fileName in req.files.file) {
     res.json(fileName)
