d16db1d85b739cbdb9fee902a7a3ad34b5273322
Zane Rockenbaugh <zane@liquid-labs.com>
2021-11-03 15:29:35 -0500

    now supports role x access CSV generation

--- src/liq-server/handlers/orgs/staff/roles/access/get.js
+++ src/liq-server/handlers/orgs/staff/roles/access/get.js
@@ -1,3 +1,5 @@
+import { format as formatCSV } from '@fast-csv/format'
+
 import { initializeRolesAccess } from './lib/roles-access-lib'
 
 const verb = 'get'
@@ -14,8 +16,52 @@ const func = ({ model }) => (req, res) => {
     res.status(500).json({ message: errors.length === 1 ? errors[0] : `* ${errors.join("\n* ")}` })
     return
   }
-  
-  res.json(rolesAccess.accessRules)
+
+  const { format = 'json' } = req.query
+  switch (format) {
+    case 'json':
+      res.json(rolesAccess.accessRules); break
+    case 'csv':
+      res.type('text/csv')
+      const csvStream = formatCSV()
+      csvStream.pipe(res)
+      
+      const domainRow = rolesAccess.domains.slice()
+      domainRow.unshift('')
+      csvStream.write(domainRow)
+      
+      const colWidth = domainRow.length
+      
+      for (const role of org.roles.list()) {
+        const row = Array.from({length: colWidth}, () => null)
+        row[0] = role.getName()
+      
+        // Fill in the rest of the row with either 'null' or an array of access rules.
+        // e.g. { domain, type, scope}
+        for (let frontierRole = role; frontierRole !== undefined; frontierRole = frontierRole.superRole) {
+          const roleName = frontierRole.getName()
+          const directAccessRules = rolesAccess.directRulesByRole[roleName]?.access || []
+          
+          // TODO: we could pre-index the build up across super-roles
+          for (const directAccessRule of directAccessRules) {
+            const { domain } = directAccessRule
+            const index = rolesAccess.getIndexForDomain(domain) + 1
+            const currCellEntries = row[index] || []
+            currCellEntries.push(directAccessRule)
+            row[index] = currCellEntries
+          }
+        }
+        
+        // csvStream.write(rolesAccess.accessRulesToSummaries(row))
+        const summary = rolesAccess.accessRulesToSummaries(row)
+        csvStream.write(summary)
+      } // end role iteration
+      csvStream.end()
+      res.end()
+      break
+    default:
+      res.status(400).json({ message: `Unsupported format '${format}'.` })
+  }
 }
 
 export { func, path, verb }
