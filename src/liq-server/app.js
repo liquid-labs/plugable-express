72d93681256365e90e8132540a888c8371dba980
Zane Rockenbaugh <zane@liquid-labs.com>
2022-01-13 11:34:28 -0600

    simplified plugins from single source

--- src/liq-server/app.js
+++ src/liq-server/app.js
@@ -1,3 +1,5 @@
+import * as path from 'path'
+
 // import asyncHandler from 'express-async-handler'
 import express from 'express'
 import fileUpload from 'express-fileupload'
@@ -19,8 +21,7 @@ const appInit = (options) => {
   reporter.log('Loading core handlers...')
   registerHandlers(app, Object.assign({}, options, { sourcePkg:'@liquid-labs/liq-core', handlers }))
   
-  loadOptionalCorePlugins(app, options)
-  loadCustomPlugins(app, options)
+  loadPlugins(app, options)
   
   return app
 }
@@ -28,61 +29,26 @@ const appInit = (options) => {
 // end "public" fuctions
 // helper fuctions below
 
+const pluginFilter = (pkgInfo) => pkgInfo.pkg.liq?.labels?.some((l) => l === PLUGIN_LABEL)
+
 /**
 *
 */
-const loadOptionalCorePlugins = (app, options) => {
-  const { reporter, skipCorePlugins = false } = options
-  if (skipCorePlugins === true) {
-    reporter.log('Skipping core plugins.')
+const loadPlugins = (app, { model, cache, reporter, skipPlugins = false }) => {
+  if (skipPlugins === true) {
+    reporter.log('Skipping plugins.')
     return
   }
   
-  reporter.log('Searching for optional core plugins...')
+  const pluginPkg = path.join(process.env.HOME, '.liq', 'plugins', 'core', 'package.json')
+  const pluginDir = path.join(process.env.HOME, '.liq', 'plugins', 'core', 'node_modules')
+  reporter.log(`Searching for plugins (in ${path.dirname(pluginDir)})...`)
   const pluginOptions = {
-    excludeDependencies: true,
-    includeOptional: true,
-    filter: pluginFilter
-  }
-  processPlugins(app, options, pluginOptions)
-}
-
-const loadCustomPlugins = (app, options) => {
-  const { reporter, customPlugins } = options
-  if (!customPlugins?.length || customPlugins.length === 0) {
-    reporter.log('No custom plugins defined.')
-    return
+    pkg: pluginPkg,
+    dir: pluginDir,
+    filter: () => true
   }
   
-  reporter.log('Searching for custom plugins...')
-  
-  for (const pluginDir of customPlugins) {
-    const pluginOptions = {
-      dir         : pluginDir,
-      scanAllDirs : true,
-      filter: pluginFilter
-    }
-    processPlugins(app, options, pluginOptions)
-  }
-}
-
-const pluginFilter = (pkgInfo) => pkgInfo.pkg.liq?.labels?.some((l) => l === PLUGIN_LABEL)
-
-const registerHandlers = (app, { sourcePkg, handlers, model, reporter, setupData, cache }) => {
-  for (const handler of handlers) {
-    const { path, method, func } = handler
-    if (path === undefined || method === undefined || func === undefined) {
-      throw new Error(`A handler from '${sourcePkg}' does not fully define 'method', 'path', and/or 'func' exports.`)
-    }
-    reporter.log(`registering handler for path: ${method.toUpperCase()}:${path}`)
-    // so express can find the handler
-    app[method](path, func({ app, cache, model, reporter, setupData }))
-    // for or own informational purposes
-    app.handlers.push({ method, path: path.toString(), sourcePkg })
-  }
-}
-
-const processPlugins = (app, { reporter, model, cache }, pluginOptions) => {
   const plugins = findPlugins(pluginOptions)
   
   console.log(plugins.length === 0 ? 'No plugins found.' : `Found ${plugins.length} plugins.`)
@@ -101,6 +67,20 @@ const processPlugins = (app, { reporter, model, cache }, pluginOptions) => {
     if (handlers !== undefined) {
       registerHandlers(app, { sourcePkg, handlers, model, reporter, setupData, cache })
     }
+  }
+}
+
+const registerHandlers = (app, { sourcePkg, handlers, model, reporter, setupData, cache }) => {
+  for (const handler of handlers) {
+    const { path, method, func } = handler
+    if (path === undefined || method === undefined || func === undefined) {
+      throw new Error(`A handler from '${sourcePkg}' does not fully define 'method', 'path', and/or 'func' exports.`)
+    }
+    reporter.log(`registering handler for path: ${method.toUpperCase()}:${path}`)
+    // so express can find the handler
+    app[method](path, func({ app, cache, model, reporter, setupData }))
+    // for or own informational purposes
+    app.handlers.push({ method, path: path.toString(), sourcePkg })
   }
 }
 
