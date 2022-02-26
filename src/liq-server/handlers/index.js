1f0ccd23c81526ce6f02790127d586955d3647d8
Zane Rockenbaugh <zane@liquid-labs.com>
2022-02-26 14:29:43 -0600

    basic 'server api' support

--- src/liq-server/handlers/index.js
+++ src/liq-server/handlers/index.js
@@ -2,11 +2,12 @@ import { handlers as orgsHandlers } from './orgs'
 import { handlers as pgHandlers } from './playground'
 
 import * as get from './get'
+import * as options from './options'
 import * as unbind from './unbind'
 
 const handlers = orgsHandlers
 handlers.push(...pgHandlers)
 
-handlers.push(get, unbind)
+handlers.push(get, options, unbind)
 
 export { handlers }
