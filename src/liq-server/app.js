6deed842a4eeb16f007d9ccd446909863a664dfa
Zane Rockenbaugh <zane@liquid-labs.com>
2022-02-28 11:15:32 -0600

    get working as es module; simplify API path, add matcher; provide better error report on bad path

--- src/liq-server/app.js
+++ src/liq-server/app.js
@@ -1,9 +1,11 @@
+import * as fs from 'fs'
 import * as path from 'path'
 
 // import asyncHandler from 'express-async-handler'
 import express from 'express'
 import fileUpload from 'express-fileupload'
 import findPlugins from 'find-plugins'
+import { pathToRegexp } from 'path-to-regexp'
 
 import { handlers } from './handlers'
 
@@ -41,7 +43,7 @@ const defaultPluginPath = path.join(process.env.HOME, '.liq', 'plugins', 'core')
 /**
 *
 */
-const loadPlugins = (app, { model, cache, reporter, skipCorePlugins = false, pluginPath = defaultPluginPath }) => {
+const loadPlugins = async (app, { model, cache, reporter, skipCorePlugins = false, pluginPath = defaultPluginPath }) => {
   if (skipCorePlugins === true) {
     reporter.log('Skipping plugins.')
     return
@@ -63,7 +65,8 @@ const loadPlugins = (app, { model, cache, reporter, skipCorePlugins = false, plu
   for (const plugin of plugins) {
     const sourcePkg = plugin.pkg.name
     reporter.log(`Loading plugins from ${sourcePkg}...`)
-    const { handlers, setup } = require(plugin.dir) || {}
+    const { main } = JSON.parse(fs.readFileSync(`${plugin.dir}/package.json`))
+    const { handlers, setup } = await import(`${plugin.dir}/${main}`) || {}
     if (handlers === undefined && setup === undefined) {
       throw new Error(`'liq-core' at least one plugin from '${sourcePkg}' does not export 'handlers' or 'setup'; bailing out.`)
     }
@@ -86,6 +89,9 @@ const registerHandlers = (app, { sourcePkg, handlers, model, reporter, setupData
     if (path === undefined || method === undefined || func === undefined) {
       throw new Error(`A handler from '${sourcePkg}' does not fully define 'method', 'path', and/or 'func' exports.`)
     }
+    if (typeof path !== 'string') {
+      throw new Error(`A handler from '${sourcePkg}' for endpoint '${path.toString()}' is not a string. Only string paths are allowed.`)
+    }
     const methodUpper = method.toUpperCase()
     reporter.log(`registering handler for path: ${methodUpper}:${path}`)
     // so express can find the handler
@@ -93,29 +99,19 @@ const registerHandlers = (app, { sourcePkg, handlers, model, reporter, setupData
     // for or own informational purposes
     const endpointDef = Object.assign({}, handler)
 
-    // convet the path to a more easily interogated data structure
-    let pathString
-    endpointDef.path = { value: path.toString() }
-    if (path instanceof RegExp) {
-      pathString = `RE: /${path.source}/${path.flags}`
-      endpointDef.path.isRegexp = true
-    }
-    else {
-      pathString = path
-    }
+    endpointDef.path = path
     
     if (!parameters) {
-      reporter.warn(`Endpoint '${pathString}' does not define 'parameters'. An explicit '[]' value should be defined where there are no parameters.`)
-      parameters = []
-      endpointDef.parameters = parameters
+      reporter.warn(`Endpoint '${method}:${path}' does not define 'parameters'. An explicit '[]' value should be defined where there are no parameters.`)
+      endpointDef.parameters = []
     }
     let i = 0
-    for (const pathParam of pathString.match(pathParamRegExp) || []) {
+    for (const pathParam of path.match(pathParamRegExp) || []) {
       const paramName = pathParam.substring(1)
-      let paramDef = parameters.find((p) => p.name === paramName)
+      let paramDef = endpointDef.parameters.find((p) => p.name === paramName)
       if (paramDef === undefined) {
         paramDef = { name: paramName }
-        parameters.push(paramDef) // TODO: I assume pushing and sorting more is quicker than unshift and sorting less
+        endpointDef.parameters.push(paramDef) // TODO: I assume pushing and sorting more is quicker than unshift and sorting less
       }
       paramDef.required = true
       paramDef.inPath = true
@@ -124,7 +120,7 @@ const registerHandlers = (app, { sourcePkg, handlers, model, reporter, setupData
       i += 1
     }
     
-    for (const paramDef of parameters) {
+    for (const paramDef of endpointDef.parameters) {
       if (paramDef.inPath === undefined && paramDef.inQuery === undefined) {
         paramDef.inQuery = true
       }
@@ -145,6 +141,15 @@ const registerHandlers = (app, { sourcePkg, handlers, model, reporter, setupData
     endpointDef.method = methodUpper
     delete endpointDef.func
     endpointDef.sourcePkg = sourcePkg // do this here so it shows up at the end of the obj
+    try {
+      // endpointDef.matcher = '^\/' + endpointDef.path.replace(pathParamRegExp, '[^/]+') + '[/#?]?$'
+      const matcher = pathToRegexp(path).toString()
+      endpointDef.matcher = matcher.substring(1, matcher.length - 2)
+    }
+    catch (e) {
+      reporter.error(`Exception while attempting to process path '${path}'. Perhaps there are special characters that need escaping; try '([*])' where '*' is your special character. Error message: ${e.message}`)
+      throw e
+    }
     
     app.handlers.push(endpointDef)
   }
