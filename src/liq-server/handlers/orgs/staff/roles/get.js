4b0578dbcb9fddf9f418761f22e9b88ef36e936f
Zane Rockenbaugh <zane@liquid-labs.com>
2022-03-21 19:44:28 -0500

    sketched in support for 'markdown'
    this is a precursor to the 'pdf' target which isolates and tests part of the process

--- src/liq-server/handlers/orgs/staff/roles/get.js
+++ src/liq-server/handlers/orgs/staff/roles/get.js
@@ -1,11 +1,44 @@
+import kebabCase from 'lodash.kebabcase'
+
 const method = 'get'
 const path = '/orgs/:orgKey/staff/roles' // TODO: shouldn't this actually end end a '/'?
+const parameters = []
 
 const func = ({ model }) => (req, res) => {
   const { orgKey } = req.params
   const org = model.orgs[orgKey]
   
-  res.json(org.roles.list())
+  const roles = org.roles.list()
+  
+  const format = req.accepts(['json','pdf','markdown']) || 'json'
+  
+  switch (format) {
+    case 'json':
+      res.json(roles); break
+    case 'pdf':
+    case 'markdown':
+      const markdown = ["# Roles\n"]
+      for (const { name, summary, superRole, implies = [] } of roles) {
+        markdown.push(
+          `## ${name}\n`,
+          "### Summary\n",
+          summary+"\n"
+        )
+        if (superRole || implies.length > 0) {
+          markdown.push("### Implies\n")
+          if (superRole && implies.findIndex((i) => i.name === superRole.name) === -1) {
+            implies.unshift({ name: superRole.name, mngrProtocol: 'self' })
+          }
+          for (const { name: impliedName } of implies) {
+            markdown.push(`- [${impliedName}](${kebabCase(impliedName)})`)
+          }
+          markdown.push("\n")
+        }
+      }
+      res.type('text/markdown').send(markdown.join("\n"))
+      break
+  }
+  
 }
 
-export { func, path, method }
+export { func, method, parameters, path }
