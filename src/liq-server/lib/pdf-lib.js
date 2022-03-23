4bcbf980aef4d5e6c661c9b6a4cab6d98e277059
Zane Rockenbaugh <zane@liquid-labs.com>
2022-03-22 20:22:48 -0500

    check 'md2x' status for errors; lint fixes

--- src/liq-server/lib/pdf-lib.js
+++ src/liq-server/lib/pdf-lib.js
@@ -1,10 +1,14 @@
 import shell from 'shelljs'
 
-const md2x = ({ markdown, format='pdf', title='Report' }) => {
+const md2x = ({ markdown, format = 'pdf', title = 'Report' }) => {
   const result = shell
     .ShellString(markdown)
-    .exec(`$(npm bin)/md2x --title '${title}' --output-format ${format} -`, { silent: true, shell: '/bin/bash' })
-    
+    .exec(`$(npm bin)/md2x --title '${title}' --output-format ${format} -`, { silent : true, shell : '/bin/bash' })
+
+  if (result.code !== 0) {
+    throw new Error(`Could not covert file to '${format}': (${result.code}) ${result.stdout}`)
+  }
+
   return `${title}.${format}`
 }
 
