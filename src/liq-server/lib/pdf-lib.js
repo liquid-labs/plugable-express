caa4210deb3ecc62f6b048a44ba1d0eb029ecf96
Zane Rockenbaugh <zane@liquid-labs.com>
2022-03-23 22:31:50 -0500

    lint fixes

--- src/liq-server/lib/pdf-lib.js
+++ src/liq-server/lib/pdf-lib.js
@@ -1,19 +1,19 @@
 import shell from 'shelljs'
 
-const md2x = ({ markdown, format='pdf', title='Report', singlePage=false, sources }) => {
+const md2x = ({ markdown, format = 'pdf', title = 'Report', singlePage = false, sources }) => {
   const sourceSpec = `${sources ? `'${sources.join("' '")}'` : '-'}`
   const options = `--list-files --title '${title}' --output-format ${format} ${singlePage ? '--single-page ' : ''}`
   const command =
     `$(npm bin)/md2x ${options} ${sourceSpec}`
   const result = shell
     .ShellString(markdown)
-    .exec(command, { silent: true, shell: '/bin/bash' })
-  
+    .exec(command, { silent : true, shell : '/bin/bash' })
+
   if (result.code !== 0) {
     throw new Error(`Could not covert file to '${format}': (${result.code}) ${result.stdout}`)
   }
-    
-  return result.toString().split("\n").filter((f) => f.length > 0)
+
+  return result.toString().split('\n').filter((f) => f.length > 0)
 }
 
 export { md2x }
