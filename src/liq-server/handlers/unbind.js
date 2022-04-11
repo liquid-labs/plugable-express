3adb2d95ced90c97cdd2e679a53100fc5b85666f
Zane Rockenbaugh <zane@liquid-labs.com>
2022-04-11 17:08:14 -0500

    upgrade to express 5-beta to solve path problems

--- src/liq-server/handlers/unbind.js
+++ src/liq-server/handlers/unbind.js
@@ -1,5 +1,5 @@
 const method = 'unbind'
-const path = '/:command((?:q|quit)?)' // '/(quit)?' breaks express 4.17.1 which uses path-to-regexp 0.1.17 (?!)
+const path = '/(quit)?'
 
 const func = () => (req, res) => {
   res.send('Shutting down...')
