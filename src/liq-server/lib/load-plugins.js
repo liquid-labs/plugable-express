79f23765b284bcc12c944fc1162b7abc040d7ba9
Zane Rockenbaugh <zane@liquid-labs.com>
2022-05-03 09:17:05 -0500

    fix merge error

--- src/liq-server/lib/load-plugins.js
+++ src/liq-server/lib/load-plugins.js
@@ -11,16 +11,7 @@ const pluginFilter = (pkgInfo) => pkgInfo.pkg.liq?.labels?.some((l) => l === PLU
 /**
 *
 */
-<<<<<<< HEAD
-const loadPlugins = async (app, { model, cache, reporter, skipCorePlugins = false, pluginPath = defaultPluginPath }) => {
-  if (skipCorePlugins === true) {
-    reporter.log('Skipping plugins.')
-    return
-  }
-  
-=======
 const loadPlugins = async (app, { model, cache, reporter, pluginPath = defaultPluginPath }) => {
->>>>>>> master
   const pluginPkg = path.join(pluginPath, 'package.json')
   const pluginDir = path.join(pluginPath, 'node_modules')
   reporter.log(`Searching for plugins (in ${path.dirname(pluginDir)})...`)
@@ -40,11 +31,7 @@ const loadPlugins = async (app, { model, cache, reporter, pluginPath = defaultPl
     const { main } = JSON.parse(fs.readFileSync(`${plugin.dir}/package.json`))
     const { handlers, setup } = await import(`${plugin.dir}/${main}`) || {}
     if (handlers === undefined && setup === undefined) {
-<<<<<<< HEAD
-      throw new Error(`'liq-core' at least one plugin from '${sourcePkg}' does not export 'handlers' or 'setup'; bailing out.`)
-=======
       throw new Error(`'liq-core' plugin from '${sourcePkg}' does not export 'handlers' or 'setup'; bailing out.`)
->>>>>>> master
     }
     
     if (setup) reporter.log(`Running setup for ${sourcePkg} plugins...`)
@@ -57,9 +44,6 @@ const loadPlugins = async (app, { model, cache, reporter, pluginPath = defaultPl
 }
 
 export {
-<<<<<<< HEAD
-=======
   defaultPluginPath,
->>>>>>> master
   loadPlugins
 }
