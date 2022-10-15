b575ea0c7ae9b87bb93ee2cd57b0366df00421cc
Zane Rockenbaugh <zane@liquid-labs.com>
2022-10-15 10:47:31 -0500

    clarified file name

--- src/liq-server/handlers/server/index.mjs
+++ src/liq-server/handlers/server/index.mjs
@@ -1,8 +1,8 @@
+import * as api from './api'
 import * as get from './get'
-import * as options from './options'
 import * as reload from './reload'
 import * as stop from './stop'
 
-const handlers = [ get, options, reload, stop ]
+const handlers = [ api, get, reload, stop ]
 
 export { handlers }
