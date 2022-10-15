2c36d7d669387ed8e0bb165100ae264be9275619
Zane Rockenbaugh <zane@liquid-labs.com>
2022-10-15 18:51:40 -0500

    detect and show options as 'next commands'

--- src/liq-server/handlers/server/next-commands.mjs
+++ src/liq-server/handlers/server/next-commands.mjs
@@ -1,40 +1,55 @@
+import omit from 'lodash.omit'
+
 const method = 'get'
 const path = [ 'server', 'next-commands' ]
 const parameters = [
   {
-    name: 'commandPath',
-    description: 'The (URL) path so far.'
+    name: 'command',
+    description: "The (URL) path so far and optional '--' plus options."
   }
 ]
 
 const func = ({ app }) => (req, res) => {
-  const { commandPath = '/' } = req.vars
+  const { command = '/'} = req.vars
+  
+  const [ commandPath, optionString ] = command.split(/\s*--\s*/)
   
   let frontier = app.commandPaths
-  const walked = []
-  const left = commandPath.split('/')
-  left.shift() // drop '' from leading '/'
-  while (left.length > 0) {
-    const command = left.shift()
+  const cmdsWalked = []
+  const cmdsLeft = commandPath.split('/')
+  cmdsLeft.shift() // drop '' from leading '/'
+  while (cmdsLeft.length > 0) {
+    const command = cmdsLeft.shift()
     if (command === '') break;
     
-    walked.push(command)
+    cmdsWalked.push(command)
     if (command in frontier) {
       frontier = frontier[command]
     }
     else {
-      res.status(400).json({ message: `Unknown terminal command of: '${walked.join("', '")}'.`})
+      res.status(400).json({ message: `Unknown terminal command of: '${cmdsWalked.join("', '")}'.`})
       return
     }
   }
   
-  const nextCommands = Object.keys(frontier)
-  if (nextCommands.length === 0) {
-    res.json(['--'])
+  const nextCommands = Object.keys(frontier).sort()
+  if (nextCommands[0] === '_parameters') {
+    
+    if (optionString === undefined) {
+      nextCommands.splice(0, 1, '--')
+    }
+    else if (optionString || optionString === '') {
+      const currOptions = optionString.match(/[a-zA-Z0-9]+(?:=(?:'[^']*'|"[^"]*"|\S+|\s+))?/g) || []
+      const currOptNames = currOptions.map((o) => o.split('=')[0])
+      const options = (frontier['_parameters'] || []).map((o) => o.name)
+      const remainder = options.filter((o) => !currOptNames.includes(o.split('=')[0]))
+      remainder.sort()
+      
+      nextCommands.splice(0, nextCommands.length, ...remainder)
+    }
   }
-  else {
-    res.json(nextCommands)
-  }
+
+  res.json(nextCommands)
 }
 
 export { func, method, parameters, path }
