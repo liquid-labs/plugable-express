6daf1408fe65ef47a1efc4ce5056913148ae9ae0
Zane Rockenbaugh <zane@liquid-labs.com>
2022-11-24 06:31:44 -0600

    fixed missing check of results in test code

--- src/liq-server/handlers/server/test/next-commands.test.js
+++ src/liq-server/handlers/server/test/next-commands.test.js
@@ -17,15 +17,18 @@ describe('GET:/server/next-commands', () => {
   
   test.each([
     [ '/', [ 'orgs', 'server' ]],
+    [ '/', [ 'orgs', 'server' ]],
     [ '/orgs', [ 'list', 'orgA' ]],
     [ '/orgs/list', [ '--' ]],
-    [ '/orgs/list --', [ 'all', 'fields' ]],
-    [ '/orgs/list -- all', [ 'fields' ]],
+    [ '/orgs/list --', [ 'fields', 'format', 'noHeaders', 'output' ]],
+    [ '/orgs/list -- fields', [ 'format', 'noHeaders', 'output' ]],
     [ '/orgs/orgA', [ 'staff' ]]
   ])('%s -> %p', async (command, nextCommands) => {
     const { status, body, headers } = await request(app)
       .get('/server/next-commands') // it reads weird, but this MUST go first
       .query({ command })
       .set('Accept', 'application/json')
+      
+      expect(body).toEqual(nextCommands)
   })
 })
