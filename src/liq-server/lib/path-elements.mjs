7a4e55942f04587cca63a86a47f0082f846a1bd8
Zane Rockenbaugh <zane@liquid-labs.com>
2022-11-28 10:01:37 -0600

    fix to correctly support the 'no arguments' case; fix orgs regex matcher

--- src/liq-server/lib/path-elements.mjs
+++ src/liq-server/lib/path-elements.mjs
@@ -1,7 +1,7 @@
 import { emailEncodedOrNotReString } from '@liquid-labs/regex-repo'
 
 const orgKey = ({ model }) => {
-  const bitReString = '[a-z0-9][a-z0-9-]*'
+  const bitReString = '[a-zA-Z0-9][a-zA-Z0-9-]*'
   const optionsFetcher = () => Object.keys(model.orgs)
   // const selectionFetcher = (key) => model.orgs[key]
   
@@ -10,7 +10,7 @@ const orgKey = ({ model }) => {
 
 const staffKey = ({ model, prevElements }) => {
   const { orgKey } = prevElements || {} // when called in the initial setup, 'prevElements' is not provided
-  const org = model.orgs[orgKey]
+  const org = model?.orgs[orgKey]
   
   const bitReString = emailEncodedOrNotReString.slice(1, -1) // cut off the beginning (and ending pins
   const optionsFetcher = () => org.staff.list({ rawData : true }).map((s) => s.id )
