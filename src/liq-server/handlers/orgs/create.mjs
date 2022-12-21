21db8fdf53856d3fad930df1da299abb04719e9d
Zane Rockenbaugh <zane@liquid-labs.com>
2022-12-21 12:58:50 -0600

    partially tested orgs create

---
+++ src/liq-server/handlers/orgs/create.mjs
@@ -0,0 +1,45 @@
+import * as fs from 'node:fs/promises'
+
+import { Organization } from '@liquid-labs/orgs-model'
+
+const method = 'post'
+const path = [ 'orgs', ':newOrgKey', 'create?' ]
+const parameters = [
+  {
+    name: 'commonName',
+    required: true,
+    description: 'The comman name by which the organization is referred to in casual speech.'
+  },
+  {
+    name: 'legalName',
+    description: 'The organizations legal name, if any.'
+  },
+  {
+    name: 'localDataRoot',
+    required: true,
+    description: 'The local directory in which to save `./orgs/org.json` for the newly created organization.'
+  }
+]
+
+const mdFormatter = (orgs, title) =>
+  `# ${title}\n\n${orgs.map((o) => `* ${o.name}`).join("\n")}\n`
+
+const func = ({ app }) => {
+  app.addCommonPathResolver('newOrgKey', {
+    bitReString: '[a-zA-Z0-9][a-zA-Z0-9-]*',
+    optionsFetcher: () => []
+  })
+
+  return async (req, res) => {
+    const { commonName, legalName, localDataRoot, newOrgKey } = req.vars
+    const localRootDir = localDataRoot + '/orgs'
+
+    await fs.mkdir(localRootDir, { recursive: true })
+
+    const rootFile = localRootDir + '/org.json'
+
+    const newOrg = Organization.initializeOrganization({ dataPath: localDataRoot, commonName, legalName, orgKey: newOrgKey })
+  }
+}
+
+export { func, parameters, path, method }
--- src/liq-server/handlers/orgs/index.js
+++ src/liq-server/handlers/orgs/index.js
@@ -1,9 +1,11 @@
 import { handlers as projectHandlers } from './projects'
 import { handlers as staffHandlers } from './staff'
 
+import * as orgCreate from './create'
 import * as orgList from './list'
 
 const handlers = [
+	orgCreate,
 	orgList,
 	...projectHandlers,
 	...staffHandlers
