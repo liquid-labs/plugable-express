39586d4fcedeffb8cb46f03871a584799db12e7a
Zane Rockenbaugh <zane@liquid-labs.com>
2023-01-14 10:28:56 -0600

    added new filter options on staff

--- src/liq-server/handlers/orgs/staff/list.js
+++ src/liq-server/handlers/orgs/staff/list.js
@@ -1,5 +1,11 @@
 import { commonOutputParams, formatOutput, getOrgFromKey } from '@liquid-labs/liq-handlers-lib'
 
+const help = {
+  name: "Staff list",
+  summary: "Retrieves a list of staff members.",
+  description: "By default, returns a list of all staff members except 'logical' staff' (such as placeholder positions). Use parameter 'all' to retrieve all staff records. Additional parameters may be specified to select a subset of staff based on roles."
+}
+
 const method = 'get'
 const path = [ 'orgs', ':orgKey', 'staff', 'list?' ]
 
@@ -18,9 +24,14 @@ const parameters = [
     optionsFunc: rolesFetcher
   },
   {
+    name: 'withAnyRole',
+    isMultivalue: true,
+    descirption: "If specified, only staff with any of the indicated roles are included. May specify multiple parameter multiple times and/or separate roles by comma. May be combined with 'withRole'."
+  },
+  {
     name: 'withRole',
     isMultivalue: true,
-    description: "If specified, only staff with all the indicated roles are included in the results. May specify multiple times and/or separate roles by comma.",
+    description: "If specified, only staff with all the indicated roles are included in the results. May specify multiple times and/or separate roles by comma. May be combined with 'withAnyRole'.",
     optionsFunc: rolesFetcher
   },
   ...commonOutputParams
@@ -40,32 +51,46 @@ const func = ({ model, reporter }) => (req, res) => {
   if (org === false) {
     return
   }
-  const { all=false, notRole, withRole } = req.vars
+  const { all=false, notRole, withAnyRole, withRole } = req.vars
 
   let staff
-  if (withRole === undefined && notRole === undefined) {
+  if (withRole === undefined && withAnyRole === undefined && notRole === undefined) {
+    // then raw data is good enough
     staff = org.staff.list({ clean: true, rawData: true }) // raw data good enough
   }
-  else {
-    staff = withRole === undefined
-      ? org.staff.list()
-      : org.staff.getByRoleName(withRole[0])
-  }
-  
-  if (withRole && withRole.length > 0 || notRole) {
-    staff = staff.filter((s) => {
-      for (let i = 1; i < withRole?.length; i+= 1) {
-        if (!s.hasRole(withRole[i])) return false
-      }
-      for (let i = 0; i < notRole?.length; i += 1) {
-        if (s.hasRole(notRole[i])) return false
-      }
-      return true
-    })
+  else { // then we'll need ful on Staff objects
+    staff = org.staff.list()
   }
   
   if (!all) {
     staff = staff.filter((s) => s.employmentStatus !== 'logical')
+  }
+
+  if (withRole !== undefined) {
+    staff = staff.filter((s) => {
+      for (const role of withRole) {
+        if (!s.hasRole(role)) return false
+      }
+      return true
+    })
+  }
+
+  if (withAnyRole !== undefined) {
+    staff = staff.filter((s) => {
+      for (const role of withAnyRole) {
+        if (s.hasRole(role)) return true
+      }
+      return false
+    })
+  }
+
+  if (notRole !== undefined) {
+    staff = staff.filter((s) => {
+      for (const role of notRole) {
+        if (s.hasRole(role)) return false
+      }
+      return false
+    })
   }
   
   formatOutput({
