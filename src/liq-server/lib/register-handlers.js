be83a254f8165ef9ac216bcdf931b8a15f1db234
Zane Rockenbaugh <zane@liquid-labs.com>
2023-01-17 10:26:05 -0600

    refactor to support multi-homed endpoints via 'paths'

--- src/liq-server/lib/register-handlers.js
+++ src/liq-server/lib/register-handlers.js
@@ -123,136 +123,138 @@ const cleanReForExpress = (pathRe) => new RegExp(pathRe.toString().replaceAll(re
 
 const registerHandlers = (app, { sourcePkg, handlers, model, reporter, setupData, cache }) => {
   for (const handler of handlers) {
-    // TODO: make use of 'pathParams' and ensure conformity between the path definition and our defined pathParams
-    const { func, help, method, parameters, path/*, pathParams */ } = handler
-    if (path === undefined || method === undefined || func === undefined) {
+    const { func, help, method, parameters, path: aPath, paths } = handler
+    if ((aPath === undefined && paths === undefined)|| method === undefined || func === undefined) {
       throw new Error(`A handler from '${sourcePkg}' does not fully define 'method', 'path', and/or 'func' exports.`)
     }
-    /* TODO: see note on regexp paths at top
-    if (typeof path !== 'string') {
-      throw new Error(`A handler from '${sourcePkg}' for endpoint '${path.toString()}' is not a string. Only string paths are allowed.`)
-    } */
+    if (aPath !== undefined && paths !== undefined) {
+      throw new Error(`A handler from '${sourcePkg}' specifies both 'path' and 'paths'; specify one or the other.`)
+    }
+
     const methodUpper = method.toUpperCase()
     
     // this must come before processCommandPath to give the function the option of registering variable name parameters
     const handlerFunc = func({ parameters, app, cache, model, reporter, setupData })
-    
-    const routablePath = typeof path === 'string'
-      ? path
-      : Array.isArray(path)
-        ? cleanReForExpress(processCommandPath({ app, model, pathArr: path, parameters }))
-        : cleanReForExpress(path) // then it's a regular expression
-    reporter.log(`registering handler for path: ${methodUpper}:${routablePath}`)
-    
-    app[method](routablePath,
-                processParams({ parameters, path }),
-                handlerFunc)
-    // for or own informational purposes
-    const endpointDef = Object.assign({}, handler)
+
+    for (const path of paths || [ aPath ]) { 
+      console.log(path) // DEBUG
+      const routablePath = typeof path === 'string'
+        ? path
+        : Array.isArray(path)
+          ? cleanReForExpress(processCommandPath({ app, model, pathArr: path, parameters }))
+          : cleanReForExpress(path) // then it's a regular expression
+      reporter.log(`registering handler for path: ${methodUpper}:${routablePath}`)
+      
+      app[method](routablePath,
+                  processParams({ parameters, path }),
+                  handlerFunc)
+      // for or own informational purposes
+      const endpointDef = Object.assign({}, handler)
 
 
-    endpointDef.path = routablePath.toString()
+      endpointDef.path = routablePath.toString()
 
-    if (!parameters) {
-      reporter.warn(`Endpoint '${method}:${path}' does not define 'parameters'. An explicit '[]' value should be defined where there are no parameters.`)
-      endpointDef.parameters = []
-    }
-    
-    if (!Object.isFrozen(parameters)) { // use parameters as a proxy instead of testing each param seperately
-      let i = 0
-      // TODO: see regex path note at top
-      // Build out any missing path parameters.
-      const pathParams = typeof path === 'string'
-        ? path.match(pathParamRegExp)
-        : path.toString().match(regexParamRegExp)
-    
-      for (const pathParam of pathParams || []) {
-        const paramName = pathParam.startsWith(':')
-          ? pathParam.substring(1)
-          : pathParam.slice(2, -1)
-        let paramDef = endpointDef.parameters.find((p) => p.name === paramName)
-        if (paramDef === undefined) {
-          paramDef = { name : paramName }
-          endpointDef.parameters.push(paramDef) // TODO: I assume pushing and sorting more is quicker than unshift and sorting less
+      if (!parameters) {
+        reporter.warn(`Endpoint '${method}:${path}' does not define 'parameters'. An explicit '[]' value should be defined where there are no parameters.`)
+        endpointDef.parameters = []
+      }
+      
+      if (!Object.isFrozen(parameters)) { // use parameters as a proxy instead of testing each param seperately
+        let i = 0
+        // TODO: see regex path note at top
+        // Build out any missing path parameters.
+        const pathParams = typeof path === 'string'
+          ? path.match(pathParamRegExp)
+          : path.toString().match(regexParamRegExp)
+      
+        for (const pathParam of pathParams || []) {
+          const paramName = pathParam.startsWith(':')
+            ? pathParam.substring(1)
+            : pathParam.slice(2, -1)
+          let paramDef = endpointDef.parameters.find((p) => p.name === paramName)
+          if (paramDef === undefined) {
+            paramDef = { name : paramName }
+            endpointDef.parameters.push(paramDef) // TODO: I assume pushing and sorting more is quicker than unshift and sorting less
+          }
+          paramDef.required = true
+          paramDef.inPath = true
+          paramDef.position = i
+          paramDef.isSingleValue = true
+          i += 1
         }
-        paramDef.required = true
-        paramDef.inPath = true
-        paramDef.position = i
-        paramDef.isSingleValue = true
-        i += 1
+
+        for (const paramDef of endpointDef.parameters) {
+          if (paramDef.inPath === undefined && paramDef.inQuery === undefined) {
+            paramDef.inQuery = true
+          }
+          Object.freeze(paramDef) // the paramDef is fully specified and shouldn't be changed here on out
+        }
       }
 
-      for (const paramDef of endpointDef.parameters) {
-        if (paramDef.inPath === undefined && paramDef.inQuery === undefined) {
-          paramDef.inQuery = true
-        }
-        Object.freeze(paramDef) // the paramDef is fully specified and shouldn't be changed here on out
+      // sort path parameters first
+      if (!Object.isFrozen(endpointDef.parameters)) { // This can happen while testing or reloading
+        endpointDef.parameters.sort((a, b) => {
+          if (a.inPath === true && b.inQuery === true) {
+            return -1
+          }
+          else if (b.inPath === true && a.inQuery === true) {
+            return 1
+          }
+          else if (a.inPath) /* sort by position */ return a.position > b.position ? 1 : -1 // position is never equal
+          else /* query param; sort by name */ return a.name.localeCompare(b.name)
+        })
       }
-    }
 
-    // sort path parameters first
-    if (!Object.isFrozen(endpointDef.parameters)) { // This can happen while testing or reloading
-      endpointDef.parameters.sort((a, b) => {
-        if (a.inPath === true && b.inQuery === true) {
-          return -1
-        }
-        else if (b.inPath === true && a.inQuery === true) {
-          return 1
-        }
-        else if (a.inPath) /* sort by position */ return a.position > b.position ? 1 : -1 // position is never equal
-        else /* query param; sort by name */ return a.name.localeCompare(b.name)
-      })
-    }
-
-    // a little cleanup and annotation
-    endpointDef.method = methodUpper
-    delete endpointDef.func
-    endpointDef.sourcePkg = sourcePkg // do this here so it shows up at the end of the obj
-    try {
-      // endpointDef.matcher = '^\/' + endpointDef.path.replace(pathParamRegExp, '[^/]+') + '[/#?]?$'
-      // TODO: see regex path note at top
-      endpointDef.matcher = typeof routablePath === 'string'
-        ? pathToRegexp(routablePath).toString().slice(1, -2)
-        : routablePath.toString().slice(1, -1)
-    }
-    catch (e) {
-      reporter.error(`Exception while attempting to process path '${path}'. Perhaps there are special characters that need escaping; try '([*])' where '*' is your special character. Error message: ${e.message}`)
-      throw e
-    }
-    
-    // lockdown our internal setup
-    Object.freeze(endpointDef)
-    Object.freeze(parameters)
-    app.handlers.push(endpointDef)
-
-    if (help !== undefined) {
-      if (!Array.isArray(path)) throw new Error(`Endpoint '${path}' defines help and must use an array style path.`)
-
-      const helpPrefix = [...path]
-      helpPrefix.unshift('help')
-      // const helpPostfix = [...path]
-      // helpPostfix.push('help')
-
-      for (const helpPathTmpl of [ helpPrefix/*, helpPostfix */ ]) {
-        const helpPath = helpPathTmpl.map((b) => b.startsWith(':') ? b.slice(1) : b)
-        const routableHelpPath =
-          cleanReForExpress(processCommandPath({ app, model, pathArr: helpPath, parameters: helpParameters }))
-        const helpFunc = sendHelp({ help, method, path, parameters }) // from the main endpoint
-        app['get'](routableHelpPath,
-                    processParams({ parameters: helpParameters, path: helpPath }),
-                    helpFunc({ model, reporter }))
-
-        const helpEndpointDef = {
-           method: 'GET',
-           parameters: helpParameters,
-           path: routableHelpPath.toString(),
-           sourcePkg: sourcePkg,
-           matcher: routableHelpPath.toString().slice(1, -1)
-        }
-        Object.freeze(helpEndpointDef)
-        app.handlers.push(helpEndpointDef)
+      // a little cleanup and annotation
+      endpointDef.method = methodUpper
+      delete endpointDef.func
+      endpointDef.sourcePkg = sourcePkg // do this here so it shows up at the end of the obj
+      try {
+        // endpointDef.matcher = '^\/' + endpointDef.path.replace(pathParamRegExp, '[^/]+') + '[/#?]?$'
+        // TODO: see regex path note at top
+        endpointDef.matcher = typeof routablePath === 'string'
+          ? pathToRegexp(routablePath).toString().slice(1, -2)
+          : routablePath.toString().slice(1, -1)
       }
-    }
+      catch (e) {
+        reporter.error(`Exception while attempting to process path '${path}'. Perhaps there are special characters that need escaping; try '([*])' where '*' is your special character. Error message: ${e.message}`)
+        throw e
+      }
+      
+      // lockdown our internal setup
+      Object.freeze(endpointDef)
+      Object.freeze(parameters)
+      app.handlers.push(endpointDef)
+
+      if (help !== undefined) {
+        if (!Array.isArray(path)) throw new Error(`Endpoint '${path}' defines help and must use an array style path.`)
+
+        const helpPrefix = [...path]
+        helpPrefix.unshift('help')
+        // const helpPostfix = [...path]
+        // helpPostfix.push('help')
+
+        for (const helpPathTmpl of [ helpPrefix/*, helpPostfix */ ]) {
+          const helpPath = helpPathTmpl.map((b) => b.startsWith(':') ? b.slice(1) : b)
+          const routableHelpPath =
+            cleanReForExpress(processCommandPath({ app, model, pathArr: helpPath, parameters: helpParameters }))
+          const helpFunc = sendHelp({ help, method, path, parameters }) // from the main endpoint
+          app['get'](routableHelpPath,
+                      processParams({ parameters: helpParameters, path: helpPath }),
+                      helpFunc({ model, reporter }))
+
+          const helpEndpointDef = {
+             method: 'GET',
+             parameters: helpParameters,
+             path: routableHelpPath.toString(),
+             sourcePkg: sourcePkg,
+             matcher: routableHelpPath.toString().slice(1, -1)
+          }
+          Object.freeze(helpEndpointDef)
+          app.handlers.push(helpEndpointDef)
+        }
+      }
+    } // for (const path of paths || [ aPath ]) {...}
   } // for (const handler of handlers) {...}
 }
 
