01097fe6210ba25bd62ae43f6c2976904c2dac35
Zane Rockenbaugh <zane@liquid-labs.com>
2023-01-18 23:07:59 -0600

    implement parameter-set

--- src/liq-server/handlers/orgs/index.js
+++ src/liq-server/handlers/orgs/index.js
@@ -5,12 +5,14 @@ import * as orgCreate from './create'
 import * as orgList from './list'
 import * as paramDetail from './parameters-detail'
 import * as paramList from './parameters-list'
+import * as paramSet from './parameters-set'
 
 const handlers = [
 	orgCreate,
 	orgList,
 	paramDetail,
 	paramList,
+	paramSet,
 	...projectHandlers,
 	...staffHandlers
 ]
