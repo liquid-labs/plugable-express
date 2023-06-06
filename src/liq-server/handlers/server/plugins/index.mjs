75a1ec918f049ce4a38b2150d323d9208739566a
Zane Rockenbaugh <zane@liquid-labs.com>
2023-06-06 10:47:42 -0500

    implemented plugin details endpoint

--- src/liq-server/handlers/server/plugins/index.mjs
+++ src/liq-server/handlers/server/plugins/index.mjs
@@ -1,9 +1,10 @@
 import { handlers as registriesHandlers } from './registries'
 
 import * as addHandler from './add'
+import * as detailsHandler from './details'
 import * as listHandler from './list'
 import * as removeHandler from './remove'
 
-const handlers = [addHandler, listHandler, removeHandler, ...registriesHandlers]
+const handlers = [addHandler, detailsHandler, listHandler, removeHandler, ...registriesHandlers]
 
 export { handlers }
