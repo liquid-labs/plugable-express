14f96e1f2c2582109577ad35d8458b1209dddf2b
Zane Rockenbaugh <zane@liquid-labs.com>
2023-06-06 11:53:09 -0500

    fix mispelling in env var constant

--- src/liq-server/defaults.js
+++ src/liq-server/defaults.js
@@ -3,7 +3,7 @@ import { Reporter } from './lib/reporter'
 const LIQ_PORT = 'LIQ_PORT'
 const DEFAULT_PORT = 32600 // this number is the ASCII codes for 'l', 'i', and 'q' summed and multiplied by 100. :)
 
-const LIQ_REGISTRIES = 'LIQ_REGISTRTIES'
+const LIQ_REGISTRIES = 'LIQ_REGISTRIES'
 const DEFAULT_REGISTRIES = ['raw.githubusercontent.com/liquid-labs/liq-registry/main/registry.yaml']
 
 const defaults = {
