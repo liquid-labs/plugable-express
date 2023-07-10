c534bcb361dadc9b964e1b9192450a6de16d44cd
Zane Rockenbaugh <zane@liquid-labs.com>
2023-07-10 14:26:17 -0500

    lint formatting

--- src/liq-server/test/lib/test-utils.js
+++ src/liq-server/test/lib/test-utils.js
@@ -3,7 +3,7 @@ import * as path from 'path'
 
 import { Reporter } from '../../lib/reporter'
 
-const COMMAND_COUNT = 32
+const COMMAND_COUNT = 33
 const HELP_COUNT = 10
 
 const simplePlaygroundPath = path.join(__dirname, '..', 'data', 'playground-simple')
