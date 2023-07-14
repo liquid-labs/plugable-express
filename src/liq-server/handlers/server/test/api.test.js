f9d01071454ddc5a8cbbbd68de23f1712c39b3de
Zane Rockenbaugh <zane@liquid-labs.com>
2023-07-14 16:33:13 -0500

    fix parameter typo

--- src/liq-server/handlers/server/test/api.test.js
+++ src/liq-server/handlers/server/test/api.test.js
@@ -13,7 +13,7 @@ describe('GET:/server/api', () => {
   beforeAll(async() => {
     process.env.LIQ_PLAYGROUND = testOptions.LIQ_PLAYGROUND_PATH
     model = initModel(testOptions);
-    ({ app, cache } = await appInit(defaultTestOptions({ model, noAPIUpdate })))
+    ({ app, cache } = await appInit(defaultTestOptions({ model, noAPIUpdate: true })))
   })
 
   afterAll(() => {
