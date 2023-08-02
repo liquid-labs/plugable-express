685b1785b076c7c85ca7f9a89b5c4c7ea04a811c
Zane Rockenbaugh <zane@liquid-labs.com>
2023-08-02 14:00:45 -0500

    removed non-core path resolvers

--- src/liq-server/lib/path-resolvers.js
+++ src/liq-server/lib/path-resolvers.js
@@ -1,19 +1,6 @@
-import { emailEncodedOrNotReString } from '@liquid-labs/regex-repo'
-
 const handlerPluginName = {
   bitReString    : '[a-z][a-z0-9-]*',
   optionsFetcher : ({ app }) => app.liq.handlerPlugins.map(({ name }) => name)
-}
-
-const localProjectName = {
-  bitReString    : '[a-zA-Z0-9][a-zA-Z0-9-_]*',
-  optionsFetcher : ({ currToken = '', model, orgKey }) => {
-    const orgKeyLength = orgKey.length + 1
-    const projectNames = Object.keys(model.playground.projects)
-      .filter((p) => p.startsWith(orgKey + '/' + currToken))
-      .map((p) => p.slice(orgKeyLength))
-    return projectNames
-  }
 }
 
 const newOrgKey = {
@@ -26,16 +13,11 @@ const orgKey = {
   optionsFetcher : ({ model }) => Object.keys(model.orgs)
 }
 
-const staffKey = {
-  bitReString    : emailEncodedOrNotReString.slice(1, -1), // cut off the beginning and ending '/'
-  optionsFetcher : ({ model, orgKey }) => model.orgs[orgKey].staff.list({ rawData : true }).map((s) => s.id)
-}
-
 const threadId = {
   bitReString    : '[0-9]+',
   optionsFetcher : ({ app }) => app.tasks.list()
 }
 
-const commonPathResolvers = { localProjectName, newOrgKey, handlerPluginName, orgKey, staffKey, threadId }
+const commonPathResolvers = { localProjectName, newOrgKey, handlerPluginName, orgKey, threadId }
 
 export { commonPathResolvers }
