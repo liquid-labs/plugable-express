500284b13c1dca24a0e01a82879d639e9d34a8ea
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-30 19:02:53 -0500

    Simplify GET:/sever and throw a 'version' on the path.

--- src/handlers/server/index.mjs
+++ src/handlers/server/index.mjs
@@ -2,11 +2,11 @@ import { handlers } from './errors'
 import { handlers as pluginHandlers } from './plugins'
 
 import * as api from './api'
-import * as get from './get'
 import * as nextCommands from './next-commands'
 import * as reload from './reload'
 import * as stop from './stop'
+import * as version from './version'
 
-handlers.push(api, get, nextCommands, reload, stop, ...pluginHandlers)
+handlers.push(api, nextCommands, reload, stop, version, ...pluginHandlers)
 
 export { handlers }
